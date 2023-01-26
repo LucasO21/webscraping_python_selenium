250d8da5063a53fd37d85b3e60ff32b000fd9b14
clfo2014@gmail.com <clfo2014@gmail.com>
2023-01-26 08:36:51 -0500

    self-scrolling; complete

--- 03_selenium.py
+++ 03_selenium.py
@@ -59,9 +59,59 @@ for url in url_list:
         titles_list.append(title)
 
 
-# 
+# TEXT BOX INPUT
+
+# Setup
+url = "https://www.google.com/"
+driver.get(url)
+
+# - Input Text In a Searbox (Google)
+box = driver.find_element("xpath", '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+box.send_keys("espnsoccernet.com")
+box.send_keys(Keys.ENTER)
+
+# - Click on Buttons (Google)
+search_button = driver.find_element("xpath", '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]')
+search_button.click()
+
+link = driver.find_element("xpath", '//*[@id="rso"]/div[1]/div/div/div[1]/div/a/h3')
+link = link.click()
+
+scores = driver.find_element('xpath', '//*[@id="global-nav-secondary"]/div/ul/li[3]/a/span[1]')
+scores = scores.click()
+
+# - Taking a Screenshot 
+driver.save_screenshot("sreenshot.png")
+
+# - Taking a Screenshot (Full Example)
+driver = webdriver.Chrome("../../chrome_driver/chromedriver_mac64/chromedriver")
+url = "https://www.google.com/"
+driver.get(url)
+
+box = driver.find_element("xpath", '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+box.send_keys("arsenal fc")
+box.send_keys(Keys.ENTER)
+
+images_button = driver.find_element('xpath', '//*[@id="hdtb-msb"]/div[1]/div/div[3]/a').click()
+
+driver.find_element('xpath', '//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img').screenshot('arsenal_logo.png')
+
+# - Scrolling
+
+# Return Height of Entire Webpage
+driver.execute_script('return document.body.scrollHeight')
+
+# Scrolls to Specific Section of Page
+driver.execute_script('window.scrollTo(0, 500000)')
+
+# Scrolls From Top to Bottom
+driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+
+while True:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
    
-    
-    
+
+
+
     
     
